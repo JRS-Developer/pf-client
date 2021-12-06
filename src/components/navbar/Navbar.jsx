a9258e8853fccdffe7006cbc22ffa8c82e87c3f2
unknown <luanklett@gmail.com>
2021-12-06 18:27:02 -0300

    refactorización de css a styled-components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import {Link} from 'react-router-dom'
 import { CampaignTwoTone, KeyboardArrowDown } from '@mui/icons-material';
-import './Navbar.css';
+import { NavbarDiv } from './NavbarStyles';
 
 const Navbar = ({show, click}) => {
   const navbarClass = ["navbar"];
@@ -10,74 +10,76 @@ const Navbar = ({show, click}) => {
   }
 
   return (
-    <nav className={navbarClass.join(" ")}>
-      <ul className="navbar_links">
-        <li className="navbar_list">
-          <Link to="#">COMUNICACIÓN <KeyboardArrowDown className="icono"/></Link>
-          <ul className="navbar_sub_links">
-            <li>
-              <Link to="/">
-                <CampaignTwoTone sx={{fontSize: 28}}/>
-                <span>Noticias</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/">
-                <CampaignTwoTone sx={{fontSize: 28}}/>
-                <span>Noticias</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/">
-                <CampaignTwoTone sx={{fontSize: 28}}/>
-                <span>Noticias</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/">
-                <CampaignTwoTone sx={{fontSize: 28}}/>
-                <span>Noticias</span>
-              </Link>
-            </li>
-          </ul>
-        </li>
-        <li className="navbar_list">
-          <Link to="#">AULA VIRTUAL <KeyboardArrowDown className="icono" /></Link>
-          <ul className="navbar_sub_links">
-            <li>
-              <Link to="/">
-                <CampaignTwoTone sx={{fontSize: 28}}/>
-                <span>Noticias</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/">
-                <CampaignTwoTone sx={{fontSize: 28}}/>
-                <span>Noticias</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/">
-                <CampaignTwoTone sx={{fontSize: 28}}/>
-                <span>Noticias</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/">
-                <CampaignTwoTone sx={{fontSize: 28}}/>
-                <span>Noticias</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/roles">
-                <CampaignTwoTone sx={{fontSize: 28}}/>
-                <span>Nuevo link</span>
-              </Link>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
+    <NavbarDiv>
+      <nav className={navbarClass.join(" ")}>
+        <ul className="navbar_links">
+          <li className="navbar_list">
+            <Link to="#">COMUNICACIÓN <KeyboardArrowDown className="icono"/></Link>
+            <ul className="navbar_sub_links">
+              <li>
+                <Link to="/">
+                  <CampaignTwoTone sx={{fontSize: 28}}/>
+                  <span>Noticias</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <CampaignTwoTone sx={{fontSize: 28}}/>
+                  <span>Noticias</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <CampaignTwoTone sx={{fontSize: 28}}/>
+                  <span>Noticias</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <CampaignTwoTone sx={{fontSize: 28}}/>
+                  <span>Noticias</span>
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li className="navbar_list">
+            <Link to="#">AULA VIRTUAL <KeyboardArrowDown className="icono" /></Link>
+            <ul className="navbar_sub_links">
+              <li>
+                <Link to="/">
+                  <CampaignTwoTone sx={{fontSize: 28}}/>
+                  <span>Noticias</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <CampaignTwoTone sx={{fontSize: 28}}/>
+                  <span>Noticias</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <CampaignTwoTone sx={{fontSize: 28}}/>
+                  <span>Noticias</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/">
+                  <CampaignTwoTone sx={{fontSize: 28}}/>
+                  <span>Noticias</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/roles">
+                  <CampaignTwoTone sx={{fontSize: 28}}/>
+                  <span>Nuevo link</span>
+                </Link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </NavbarDiv>
   )
 }
 
