32bc893e14ff156564322304cb5140bc500c1707
wilmerortiz <jhak_dy@hotmail.com>
2021-12-07 10:35:24 -0500

    Se ha finalizado con las acciones del comp-profile o roles (add/edit/delete). Ademas se ha creado los components AlertDialog y ConfirmDialog

---
+++ src/components/alert/AlertDialog.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function AlertDialog({title, message, openAlert, handleCloseAlert}) {
+
+  return (
+    <div>
+      {/*
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open alert dialog
+      </Button>*/}
+      <Dialog
+        open={openAlert}
+        onClose={handleCloseAlert}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {title}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {message}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseAlert} autoFocus>
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
