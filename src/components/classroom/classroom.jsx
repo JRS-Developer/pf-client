933a694c6f7a0c07676b24470503000325e4c6b8
Juandaniele <juandaniele1@gmail.com>
2021-12-09 16:01:39 -0300

    validacion de campos correctamente

---
+++ src/components/classroom/classroom.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Container from '@mui/material/Container';
+
+export default function ClassRoom(){
+  return(
+    <Container>Hola soy ClassRoom</Container>
+  )
+}
