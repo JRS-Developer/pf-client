df842c1ec7abf1130936720e641d3dd4b3e4529d
Luan Klett <luanklett@gmail.com>
2021-12-09 14:47:46 -0300

    Varios fix a estilos de componentes y adición de varios temas en theme.js para usar en un futuro

--- src/theme.js
+++ src/theme.js
@@ -12,6 +12,9 @@ export const LightTheme = createTheme({
     info: {
       main: '#846699',
     },
+    background: {
+      paper: '#e6e6e6',
+    },
   },
 });
 
@@ -28,4 +31,124 @@ export const DarkTheme = createTheme({
       main: '#846699',
     },
   },
+});
+
+
+//------------------------------------------- Future themes
+
+export const GaiaDarkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#668C4A',
+    },
+    secondary: {
+      main: '#0367A6',
+    },
+    info: {
+      main: '#846699',
+    },
+  },
+});
+
+export const GaiaLightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#668C4A',
+    },
+    secondary: {
+      main: '#0367A6',
+    },
+    info: {
+      main: '#846699',
+    },
+    background: {
+      paper: '#e6e6e6',
+    },
+  },
+});
+
+export const HermesTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#A4222E',
+    },
+    secondary: {
+      main: '#e47f25',
+    },
+    info: {
+      main: '#F2B705',
+    },
+    background: {
+      paper: '#e6e6e6',
+    },
+  },
+});
+
+export const HecateTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#38124D',
+    },
+    secondary: {
+      main: '#5E83F2',
+    },
+    info: {
+      main: '#2196f3',
+    },
+  },
+});
+
+export const AfroditaTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#8C0F4A',
+    },
+    secondary: {
+      main: '#BF5A36',
+    },
+    info: {
+      main: '#d41e12',
+    },
+    background: {
+      paper: '#e6e6e6',
+    },
+  }
+});
+
+export const PoseidonTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#388cad',
+    },
+    secondary: {
+      main: '#87D5D0',
+    },
+    info: {
+      main: '#D9C3B0',
+    },
+  }
+});
+
+export const  ZeusTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#313C3F',
+    },
+    secondary: {
+      main: '#EEE343',
+    },
+    info: {
+      main: '#5595A7',
+    },
+    background: {
+      paper: '#e6e6e6',
+    },
+  }
 });
