b549e72a833bd091659fbf6b10fd92eaac8c28e2
ferwin <ferwin.arias@gmail.com>
2021-12-18 10:25:52 -0500

    variable de entorno para socket

--- src/components/socket.js
+++ src/components/socket.js
@@ -1,6 +1,9 @@
-
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:80');
+const {REACT_APP_SOCKET} = process.env;
+
+if(!REACT_APP_SOCKET) console.log("error con la variable de entorno de socket.io")
+
+const socket = io(REACT_APP_SOCKET);
 
 export default socket;
