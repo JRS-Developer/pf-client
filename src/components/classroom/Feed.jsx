f2ecd93b930a9eac10b61d4e9ea63d1766ad0400
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-12-20 22:03:21 -0400

    Algunas modificaciones para filtrar las publicaciones en base de la classID y materiaID

--- src/components/classroom/Feed.jsx
+++ src/components/classroom/Feed.jsx
@@ -6,74 +6,16 @@ import { useSelector, useDispatch } from 'react-redux'
 import {
   getPosts /* createPost, updatePost, deletePost, likePost, getPost */,
 } from '../../actions/post'
+import { useParams } from 'react-router-dom'
 
 export default function Feed() {
   const posts = useSelector((store) => store.postsReducer).posts
   const dispatch = useDispatch()
+  const { claseId, materiaId } = useParams()
 
   useEffect(() => {
-    /*  dispatch(createPost({
-      "title": "Prueba Dispatch",
-      "text": "Bienvenidos todos al inicio a clases",
-      "publisher_id": "ab11729b-d0f1-4976-821d-8a30c3c178e7"
-    })) */
-    /* dispatch(updatePost({
-      "title": "Bienvenidos"
-    }, "a446b611-1ef0-4290-86f1-d0054655a31a")) */
-    /* dispatch(deletePost("a446b611-1ef0-4290-86f1-d0054655a31a")) */
-    /* dispatch(likePost("a446b611-1ef0-4290-86f1-d0054655a31a")) */
-    //dispatch(getPost("93a1fcac-7b9e-47d3-86c1-9442ceaee3c0"))
-    dispatch(getPosts())
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    dispatch(getPosts(claseId, materiaId))
   }, [])
-
-  /* const data = [
-    {
-      id: 1,
-      name: 'Juan',
-      avatar: '',
-      img: photo,
-      title: 'Biología Modulo 1',
-      description: `Hola alumnos!
-      Les dejo el material que vamos a usar todo este módulo`,
-    },
-    {
-      id: 1,
-      name: 'Juan',
-      avatar: '',
-      img: photo,
-      title: 'Biología Modulo 1',
-      description: `Hola alumnos!
-      Les dejo el material que vamos a usar todo este módulo`,
-    },
-    {
-      id: 1,
-      name: 'Juan',
-      avatar: '',
-      img: photo,
-      title: 'Biología Modulo 1',
-      description: `Hola alumnos!
-      Les dejo el material que vamos a usar todo este módulo`,
-    },
-    {
-      id: 1,
-      name: 'Juan',
-      avatar: '',
-      img: photo,
-      title: 'Biología Modulo 1',
-      description: `Hola alumnos!
-      Les dejo el material que vamos a usar todo este módulo`,
-    },
-    {
-      id: 1,
-      name: 'Juan',
-      avatar: '',
-      img: photo,
-      title: 'Biología Modulo 1',
-      description: `Hola alumnos!
-      Les dejo el material que vamos a usar todo este módulo`,
-    },
-  ] */
 
   return (
     <Box sx={{ overflow: 'auto' }}>
