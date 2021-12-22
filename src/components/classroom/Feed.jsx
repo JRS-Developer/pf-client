8c0e4f9bc6affb37da4a5cb0e403cc545b84bc79
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-12-21 22:18:56 -0400

    Feat: Avanzando el componente para mostrar publicaciones, falta el poder dar like

--- src/components/classroom/Feed.jsx
+++ src/components/classroom/Feed.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
+import { Dialog } from '@mui/material/'
 import Post from './Post'
 import { useSelector, useDispatch } from 'react-redux'
 import {
@@ -9,18 +10,29 @@ import {
 import { useParams } from 'react-router-dom'
 
 export default function Feed() {
+  const [open, setOpen] = useState(false)
+  const [img, setImg] = useState(undefined)
+
   const posts = useSelector((store) => store.postsReducer).posts
   const dispatch = useDispatch()
   const { claseId, materiaId } = useParams()
 
+  const handleFull = (img) => {
+    setOpen(true)
+    setImg(img)
+  }
+
+  const handleClose = () => setOpen(false)
+
   useEffect(() => {
     dispatch(getPosts(claseId, materiaId))
-  }, [])
+  }, [claseId, materiaId, dispatch])
+  console.log(posts)
 
   return (
     <Box sx={{ overflow: 'auto' }}>
       <Grid container spacing={2}>
-        {posts &&
+        {posts?.length ? (
           posts.map((e, i) => (
             <Post
               key={`p${i}`}
@@ -28,12 +40,20 @@ export default function Feed() {
               title={e.title}
               description={e.text}
               name={`${e.publisher.firstName} ${e.publisher.lastName}`}
-              img={e.images}
+              imgs={e.images}
+							docs={e.documents}
               date={e.createdAt}
               likes={e.likes}
               madeLike={e.madeLike}
+              handleFull={handleFull}
             />
-          ))}
+          ))
+        ) : (
+          <Grid item>Sin publicaciones</Grid>
+        )}
+        <Dialog open={open} onClose={handleClose}>
+          <img src={img?.url} alt={img?.name} />
+        </Dialog>
       </Grid>
     </Box>
   )
--- src/components/classroom/Post.jsx
+++ src/components/classroom/Post.jsx
@@ -1,15 +1,20 @@
 import {
+  Box,
   Card,
   CardActions,
   CardHeader,
   CardContent,
   CardMedia,
   IconButton,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  Chip,
 } from '@mui/material/'
 import Grid from '@mui/material/Grid'
 import Avatar from '@mui/material/Avatar'
 import Typography from '@mui/material/Typography'
-import { Favorite } from '@mui/icons-material/'
+import { Favorite, Fullscreen, Article, Image } from '@mui/icons-material/'
 import { pink } from '@mui/material/colors'
 import { format } from 'date-fns'
 
@@ -18,13 +23,99 @@ export default function Post({
   title,
   description,
   name,
-  img,
+  imgs,
+  docs,
   date,
   likes,
   madeLike,
+  handleFull,
 }) {
   const favoriteProps = {}
   madeLike && (favoriteProps.sx = { color: pink[200] })
+
+  // TODO: Eliminar esta linea:
+  imgs = [...imgs, ...imgs]
+
+  const shortName = (text) => {
+    return text.substr(0, 15) + '...' + text.split('.').pop()
+  }
+
+  const showImgs = () =>
+    imgs.length && !docs.length ? (
+      imgs.length > 1 ? (
+        <ImageList cols={2}>
+          {imgs.map((img) => {
+            return (
+              <ImageListItem
+                key={img.id}
+                sx={{
+                  ':hover button': {
+                    opacity: 1,
+                    transition: 'opacity .7s',
+                  },
+                }}
+              >
+                <img src={img.url} alt={img.name} loading="lazy" />
+                <ImageListItemBar
+                  actionIcon={
+                    <IconButton
+                      sx={{
+                        opacity: 0,
+                        transition: 'opacity .7s',
+                      }}
+                      onClick={() => handleFull(img)}
+                    >
+                      <Fullscreen />
+                    </IconButton>
+                  }
+                />
+              </ImageListItem>
+            )
+          })}
+        </ImageList>
+      ) : (
+        <CardMedia component="img" image={imgs[0].url} alt={imgs[0].name} />
+      )
+    ) : null
+
+  const showFiles = () => {
+    const chipProps = {
+      component: 'a',
+      color: 'primary',
+      clickable: true,
+      rel: 'noreferrer',
+      target: '_blank',
+    }
+    return docs.length ? (
+      <Box
+        display="flex"
+        sx={{
+          flexWrap: 'wrap',
+          gap: '.5rem',
+          paddingTop: '1rem',
+        }}
+      >
+        {docs.map((doc) => (
+          <Chip
+            {...chipProps}
+            key={doc.id}
+            icon={<Article />}
+            label={shortName(doc.name)}
+            href={doc.url}
+          />
+        ))}
+        {imgs.map((img) => (
+          <Chip
+            {...chipProps}
+            key={img.id}
+            icon={<Image />}
+            label={shortName(img.name)}
+            href={img.url}
+          />
+        ))}
+      </Box>
+    ) : null
+  }
 
   return (
     <Grid item xs={12}>
@@ -34,17 +125,17 @@ export default function Post({
           title={name}
           subheader={format(new Date(date), 'MMMM, d, yyyy')}
         />
-        {img?.length && (
-          <CardMedia component="img" image={img[0]} alt={title} />
-        )}
+        {showImgs()}
         <CardContent>
+          <Typography variant="h6">{title}</Typography>
           <Typography variant="body2">{description}</Typography>
+          {showFiles()}
         </CardContent>
         <CardActions>
           <IconButton aria-label="add to favorites">
             <Favorite {...favoriteProps} />
           </IconButton>
-          {likes.length}
+          <Typography variant="body2">{likes.length}</Typography>
         </CardActions>
       </Card>
     </Grid>
