bdaf16dfbe1a2b349e24a1a90dcbf843e087895d
Leandro <leandrovillafu@gmail.com>
2021-12-28 16:42:33 -0300

    componente entregas

--- src/components/classroom/homework/Entregas.jsx
+++ src/components/classroom/homework/Entregas.jsx
@@ -1,151 +1,78 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import FolderIcon from '@mui/icons-material/Folder';
-import DeleteIcon from '@mui/icons-material/Delete';
-import AssignmentSharpIcon from '@mui/icons-material/AssignmentSharp';
+import * as React from 'react'
+import Link from '@mui/material/Link';
+import { styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemAvatar from '@mui/material/ListItemAvatar'
+import ListItemText from '@mui/material/ListItemText'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import DeleteIcon from '@mui/icons-material/Delete'
+import AssignmentSharpIcon from '@mui/icons-material/AssignmentSharp'
+import ListItemButton from '@mui/material/ListItemButton'
 
-function generate(element) {
-  return [0, 1, 2].map((value) =>
-    React.cloneElement(element, {
-      key: value,
-    }),
-  );
-}
+const tareas = [
+  {
+    id: '140fd1cd-b99f-4683-8c41-04ed84c62ce5',
+    name: 'Tarea 1',
+    description: 'Descripcion de tarea',
+  },
+  {
+    id: '5485d298-41d2-40d5-8b8b-9aaa18bca450',
+    name: 'Tarea 2',
+    description: 'Descripcion de tarea',
+  },
+  {
+    id: 'fd250d53-f74e-473b-a93a-e8a30212c243',
+    name: 'Tarea 3',
+    description: 'Descripcion de tarea',
+  },
+]
 
 const Demo = styled('div')(({ theme }) => ({
   backgroundColor: theme.palette.background.paper,
-}));
+}))
 
-export default function InteractiveList() {
-  const [dense, setDense] = React.useState(false);
-  const [secondary, setSecondary] = React.useState(false);
-
+export default function Entregas() {
   return (
     <Box sx={{ flexGrow: 1, maxWidth: 752 }}>
-      <FormGroup row>
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={dense}
-              onChange={(event) => setDense(event.target.checked)}
-            />
-          }
-          label="Enable dense"
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={secondary}
-              onChange={(event) => setSecondary(event.target.checked)}
-            />
-          }
-          label="Enable secondary text"
-        />
-      </FormGroup>
       <Grid container spacing={2}>
         <Grid item xs={12} md={6}>
           <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-            Text only
+            Tareas de esta clase y materia
           </Typography>
           <Demo>
-            <List dense={dense}>
-              {generate(
-                <ListItem>
-                  <ListItemText
-                    primary="Single-line item"
-                    secondary={secondary ? 'Secondary text' : null}
-                  />
-                </ListItem>,
-              )}
-            </List>
-          </Demo>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-            Icon with text
-          </Typography>
-          <Demo>
-            <List dense={dense}>
-              {generate(
-                <ListItem>
-                  <ListItemIcon>
-                    <FolderIcon />
-                  </ListItemIcon>
-                  <ListItemText
-                    primary="Single-line item"
-                    secondary={secondary ? 'Secondary text' : null}
-                  />
-                </ListItem>,
-              )}
-            </List>
-          </Demo>
-        </Grid>
-      </Grid>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-            Avatar with text
-          </Typography>
-          <Demo>
-            <List dense={dense}>
-              {generate(
-                <ListItem>
-                  <ListItemAvatar>
-                    <Avatar>
-                      <AssignmentSharpIcon />
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary="Single-line item"
-                    secondary={secondary ? 'Secondary text' : null}
-                  />
-                </ListItem>,
-              )}
-            </List>
-          </Demo>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-            Avatar with text and icon
-          </Typography>
-          <Demo>
-            <List dense={dense}>
-              {generate(
-                <ListItem
-                  secondaryAction={
-                    <IconButton edge="end" aria-label="delete">
-                      <DeleteIcon />
-                    </IconButton>
-                  }
-                >
-                  <ListItemAvatar>
-                    <Avatar>
-                      <FolderIcon />
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary="Single-line item"
-                    secondary={secondary ? 'Secondary text' : null}
-                  />
-                </ListItem>,
-              )}
+            <List>
+              {tareas.map((tarea, i) => {
+                return (
+                  <ListItem key={`${tarea.id+i}`}
+                    secondaryAction={
+                      <IconButton edge="end" aria-label="delete">
+                        <DeleteIcon />
+                      </IconButton>
+                    }
+                  >
+                    <Link underline="none" href={`/entregas/${tarea.id}`}>
+                    <ListItemButton>
+                      <ListItemAvatar>
+                        <Avatar>
+                          <AssignmentSharpIcon />
+                        </Avatar>
+                      </ListItemAvatar>
+
+                      <ListItemText primary={tarea.name} secondary={tarea.description} />
+                    </ListItemButton>
+                    </Link>
+                  </ListItem>
+                )
+              })}
             </List>
           </Demo>
         </Grid>
       </Grid>
     </Box>
-  );
+  )
 }
---
+++ src/components/classroom/homework/TablaEntregas.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TablaEntregas = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TablaEntregas
