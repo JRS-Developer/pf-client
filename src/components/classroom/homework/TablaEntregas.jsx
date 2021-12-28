2451787d55c1f3ba75bf038b0a4bb391cc52ff98
Leandro <leandrovillafu@gmail.com>
2021-12-28 18:35:38 -0300

    Planteo de componente de entregas de notas

--- src/components/classroom/homework/TablaEntregas.jsx
+++ src/components/classroom/homework/TablaEntregas.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import * as React from 'react'
+import { DataGrid } from '@mui/x-data-grid'
+import { Box, Paper } from '@mui/material'
 
-const TablaEntregas = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+const columns = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'Nombre', width: 130 },
+  { field: 'lastName', headerName: 'Apellido', width: 130 },
+  { field: 'fechaDeEntrega', headerName: 'fecha de entrega', width:130},
+  { field: 'estado', headerName:'estado', width:130}
+
+]
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', fechaDeEntrega: '28-12-21 17:20', estado:'pendiente'},
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', fechaDeEntrega: '28-12-21 17:20' , estado:'aprobado'},
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', fechaDeEntrega: '28-12-21 17:20', estado:'rechazado' },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', fechaDeEntrega: '28-12-21 17:20', estado:'aprobado' },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', fechaDeEntrega: null , estado:'pendiente'},
+  { id: 6, lastName: 'Melisandre', firstName: null, fechaDeEntrega: null, estado:'pendiente' },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', fechaDeEntrega: '28-12-21 17:20', estado:'pendiente' },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', fechaDeEntrega: '28-12-21 17:20', estado:'pendiente' },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', fechaDeEntrega: '28-12-21 17:20' , estado:'pendiente'},
+]
+
+export default function TablaEntregas() {
+  return (
+    <Box>
+      <Box>
+        <h3>{`Entregas`}</h3>
+      </Box>
+      <Paper elevation={24}>
+        <Box sx={{ height: 'calc(100vh - 132px)', width: 1 }}>
+          <DataGrid
+            sx={{
+              borderRadius: 2,
+              boxShadow: 3,
+              border: 0,
+              borderColor: 'primary.light',
+              '& .MuiDataGrid-cell:hover': {
+                color: 'primary.main',
+              },
+            }}
+            rows={rows}
+            columns={columns}
+            pageSize={30}
+            rowsPerPageOptions={[5]}
+            checkboxSelection={false}
+          />
+        </Box>
+      </Paper>
+    </Box>
+  )
 }
-
-export default TablaEntregas
