d5d5f853f8509f060c43a776da3c980de110523e
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-12-29 20:32:13 -0400

    Fix: Actualizado el form de classes, ahora se pueden seleccionar varias escuelas o sucursales y la tabla ahora muestra correctamente todas las escuelas en una columna schools

--- src/components/class/ClassIndex.jsx
+++ src/components/class/ClassIndex.jsx
@@ -1,30 +1,40 @@
-import React, {useEffect} from "react";
-import { useSelector, useDispatch } from "react-redux";
-import ClassForm from "./ClassForm";
-import Table from "../Table/Table"
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import ClassForm from './ClassForm'
+import Table from '../Table/Table'
 
 //importamos los métodos
-import { getClases as listClass, getDataById, modifiedClase } from "../../actions/clase";
+import {
+  getClases as listClass,
+  getDataById,
+  modifiedClase,
+} from '../../actions/clase'
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 350 },
-  { field: 'name', headerName: 'Name', width: 300},
-  { field: 'school', headerName: 'School', width: 300},
-  { field: 'status', headerName: 'Status Clase', width: 300},
-
-];
+  { field: 'name', headerName: 'Name', width: 300 },
+  {
+    field: 'schools',
+    headerName: 'Schools',
+    width: 300,
+    valueGetter: (params) => {
+      return params.row.schools?.map((school) => school.name).join(', ') || ''
+    },
+  },
+  { field: 'status', headerName: 'Status Clase', width: 300 },
+]
 
 const form = ClassForm
 
 export default function ClassIndex() {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-  const getStatusReducer = useSelector(state => state.clasesReducer);
-  const { clases, /* loading, error */ } = getStatusReducer;
+  const getStatusReducer = useSelector((state) => state.clasesReducer)
+  const { clases /* loading, error */ } = getStatusReducer
 
   const data = {
     columns,
-    rows: clases
+    rows: clases,
   }
 
   useEffect(() => {
@@ -33,15 +43,17 @@ export default function ClassIndex() {
 
   return (
     <>
-      { <Table
-        data={data}
-        DialogForm={form}
-        title="CLASES"
-        getDataById={getDataById}
-        getActions={getStatusReducer}
-        modifiedAction={modifiedClase}
-        listData={listClass}
-      /> }
+      {
+        <Table
+          data={data}
+          DialogForm={form}
+          title="CLASES"
+          getDataById={getDataById}
+          getActions={getStatusReducer}
+          modifiedAction={modifiedClase}
+          listData={listClass}
+        />
+      }
     </>
   )
 }
