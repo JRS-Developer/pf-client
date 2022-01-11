bbcdc8ea44a7de363f545f8786e08ebd4ce8bbf0
Luan Klett <luanklett@gmail.com>
2022-01-11 12:44:27 -0300

    modifique algunos estilos en homeworks

--- src/components/classroom/Homeworks.jsx
+++ src/components/classroom/Homeworks.jsx
@@ -9,6 +9,7 @@ import ListItemText from '@mui/material/ListItemText'
 import Avatar from '@mui/material/Avatar'
 import AssignmentSharpIcon from '@mui/icons-material/AssignmentSharp'
 import Typography from '@mui/material/Typography'
+import Divider from '@mui/material/Divider'
 import { useParams, useHistory } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
@@ -50,7 +51,7 @@ export default function Homeworks() {
           align="center"
           sx={{ flexDirection: 'column' }}
         >
-          <Typography variant="h6">Tareas sin entregar</Typography>
+          <Typography variant="h5">Tareas sin entregar</Typography>
           <Paper display="flex" sx={{ flexDirection: 'column' }}>
             {/* {tareas.filter(t => t.complete === false).map((t, i) => {return ( */}
             {tasks &&
@@ -62,12 +63,12 @@ export default function Homeworks() {
                   return (
                     <ListItem
                       key={`${t.id + i}`}
-                      sx={{ p: 0 }}
+                      sx={{ p: 0, border: 1, borderColor: "primary.main", pr: 1 }}
                       onClick={() => handleClick(t.id)}
                     >
-                      <ListItemButton>
-                        <ListItemAvatar>
-                          <Avatar>
+                      <ListItemButton sx={{p: 1}}>
+                        <ListItemAvatar sx={{pl:1}}>
+                          <Avatar sx={{width: 35, height: 35}}>
                             <AssignmentSharpIcon />
                           </Avatar>
                         </ListItemAvatar>
@@ -75,41 +76,46 @@ export default function Homeworks() {
                         <ListItemText
                           disableTypography
                           primary={
+                            <Box>
                             <Typography
+                            variant="h6"
                               sx={{
                                 textOverflow: 'ellipsis',
                                 overflow: 'hidden',
                                 whiteSpace: 'nowrap',
-                                width: 'calc(46vw - 200px)',
+                                width: 'calc(46vw - 200px)'
                               }}
                             >
                               {t.title}
                             </Typography>
+                            <Divider sx={{pb: 0.5}}/>
+                            </Box>
                           }
                           secondary={
-                            <Box sx={{
-                              display: 'flex',
-                              justifyContent: 'space-between'
-                            }}>
+                            <Grid container>
+                              <Grid item xs={8}>
                             <Typography
+                            variant="body2"
                               sx={{
-                                textOverflow: 'ellipsis',
-                                overflow: 'hidden',
-                                whiteSpace: 'nowrap',
-                                width: 'calc(46vw - 350px)',
+                                pt: 0.5,
+                                width: '100%',
+                                lineHeight: "1.5em",
+                                height: "4.5em",
+                                overflow: "hidden",
+                                textOverflow: "ellipsis",
+                                whiteSpace: "normal",
                               }}
                             >
                               {t.description}
                             </Typography>
-                            <Typography
-                            sx={{
-                              marginRight: '10px'
-                            }}
-                           >
+                            </Grid>
+                            <Grid item xs={4}>
+                            <Typography variant="body2" sx={{pt: 0.5, textAlign: "right"}}>
                      
-                             {'Fecha de entrega: ' + format(new Date(t.end_date), 'dd/MM/yy')}
+                             {'Fecha de entrega:'} <br/> {format(new Date(t.end_date), 'dd/MM/yy')}
                            </Typography>
-                           </Box>
+                           </Grid>
+                           </Grid>
                           }
                           
                         />
@@ -127,7 +133,7 @@ export default function Homeworks() {
           align="center"
           sx={{ flexDirection: 'column' }}
         >
-          <Typography variant="h6">Tareas entregadas</Typography>
+          <Typography variant="h5">Tareas entregadas</Typography>
           <Paper display="flex" sx={{ flexDirection: 'column' }}>
             {/* {tareas.filter(t => t.complete === false).map((t, i) => {return ( */}
             {tasks &&
@@ -139,12 +145,12 @@ export default function Homeworks() {
                   return (
                     <ListItem
                       key={`${t.id + i}`}
-                      sx={{ p: 0 }}
+                      sx={{ p: 0, border: 1, borderColor: "primary.main", pr: 1 }}
                       onClick={() => handleClick(t.id)}
                     >
-                      <ListItemButton>
-                        <ListItemAvatar>
-                          <Avatar>
+                      <ListItemButton sx={{p: 1}}>
+                        <ListItemAvatar sx={{pl:1}}>
+                          <Avatar sx={{width: 35, height: 35}}>
                             <AssignmentSharpIcon />
                           </Avatar>
                         </ListItemAvatar>
@@ -152,30 +158,50 @@ export default function Homeworks() {
                         <ListItemText
                           disableTypography
                           primary={
+                            <Box>
                             <Typography
+                            variant="h6"
                               sx={{
                                 textOverflow: 'ellipsis',
                                 overflow: 'hidden',
                                 whiteSpace: 'nowrap',
-                                width: 'calc(46vw - 200px)',
+                                width: 'calc(46vw - 200px)'
                               }}
                             >
                               {t.title}
                             </Typography>
+                            <Divider sx={{pb: 0.5}}/>
+                            </Box>
                           }
                           secondary={
+                            <Grid container>
+                              <Grid item xs={8}>
                             <Typography
+                            variant="body2"
                               sx={{
-                                textOverflow: 'ellipsis',
-                                overflow: 'hidden',
-                                whiteSpace: 'nowrap',
-                                width: 'calc(46vw - 200px)',
+                                pt: 0.5,
+                                width: '100%',
+                                lineHeight: "1.5em",
+                                height: "4.5em",
+                                overflow: "hidden",
+                                textOverflow: "ellipsis",
+                                whiteSpace: "normal",
                               }}
                             >
                               {t.description}
                             </Typography>
+                            </Grid>
+                            <Grid item xs={4}>
+                            <Typography variant="body2" sx={{pt: 0.5, textAlign: "right"}}>
+                     
+                             {'Fecha de entrega:'} <br/> {format(new Date(t.end_date), 'dd/MM/yy')}
+                           </Typography>
+                           </Grid>
+                           </Grid>
                           }
+                          
                         />
+
                       </ListItemButton>
                     </ListItem>
                   )
