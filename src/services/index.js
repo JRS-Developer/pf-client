f6a2b1962f6bf3b986594f9d370828c143f270e0
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-01-11 17:27:52 -0400

    Test

--- src/services/index.js
+++ src/services/index.js
@@ -1,20 +1,17 @@
-import axios from 'axios';
-import * as serviceWorkerRegistration from '../serviceWorkerRegistration';
-const { REACT_APP_WEBPUSH, PUBLIC_VAPID_KEY} = process.env;
+import axios from 'axios'
+import * as serviceWorkerRegistration from '../serviceWorkerRegistration'
+const { REACT_APP_WEBPUSH, PUBLIC_VAPID_KEY } = process.env
+const user = localStorage.getItem('user')
 
 export const subscription = async (data) => {
-    
-    const reg = await serviceWorkerRegistration.register()
+  const reg = await serviceWorkerRegistration.register()
 
-    const suscribe = await reg.pushManager.subscribe({
-        userVisibleOnly: true,
-        applicationServerKey: PUBLIC_VAPID_KEY.replace,
-    });
+  const suscribe = await reg.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: PUBLIC_VAPID_KEY.replace,
+  })
 
+  await axios.post(`${REACT_APP_WEBPUSH}/subscription`, { sub: suscribe, user })
 
-    await axios.post(REACT_APP_WEBPUSH, suscribe);
-
-    console.log('suscribed!')
-
+  console.log('suscribed!')
 }
-
