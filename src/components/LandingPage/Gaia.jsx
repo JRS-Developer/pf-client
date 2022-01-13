c22fdf8562662c259f91ee16ed25dcbcc893e0d5
Nliasso <nliasso@hotmail.com>
2022-01-12 21:30:08 -0300

    inicios de la refactorización de la Landing Page

--- src/components/LandingPage/Gaia.jsx
+++ src/components/LandingPage/Gaia.jsx
@@ -4,152 +4,179 @@ import Typography from '@mui/material/Typography'
 import Slide from '@mui/material/Slide'
 import Paper from '@mui/material/Paper'
 
-export default function Gaia({scroll}){
-
+export default function Gaia({ scroll }) {
   return (
-  <Grid container>
-    <Slide in={true} direction="up">
-      <Grid
-        item
-        xs={4}
-        sx={{
-          p: 5,
-          pr: 2.5,
-          pb: 2.5,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <img src={Logo} style={{ width: '100%' }} alt="Logo Gaia"/>
-      </Grid>
-    </Slide>
-    <Slide in={true} timeout={500} direction="left">
-      <Grid
-        item
-        xs={8}
-        sx={{ p: 5, pl: 2.5, pb: 2.5, display: 'flex', alignItems: 'center' }}
-      >
-        <Paper sx={{ width: '100%', height: '100%', p: 2 }}>
-          <Typography variant="h5">¿Que es Gaia?</Typography>
-          <Typography variant="h6" noWrap component="div">
-            <br />
-            <p>Gaia es...</p>
-            <p>a</p>a<p>a</p>
-            <p>a</p>
-            <p>a</p>
-          </Typography>
-        </Paper>
-      </Grid>
-    </Slide>
-    <Slide in={true} timeout={500} direction="up">
-      <Grid
-        item
-        xs={7}
-        sx={{ p: 2.5, pl: 5, display: 'flex', alignItems: 'center' }}
-      >
-        <Paper sx={{ width: '100%', height: '100%', p: 2 }}>
-          <Typography variant="h5">¿Otra cosa?</Typography>
-          <Typography variant="h6" noWrap component="div">
-            <br />
-            <p>Otras cosas</p>
-            <p>a</p>a<p>a</p>
-            <p>a</p>
-            <p>a</p>
-          </Typography>
-        </Paper>
-      </Grid>
-    </Slide>
-    <Slide in={true} timeout={500} direction="right">
-      <Grid
-        item
-        xs={5}
-        sx={{ p: 2.5, pr: 5, display: 'flex', alignItems: 'center' }}
-      >
-        <Paper sx={{ width: '100%', height: '100%', p: 2 }}>
-          <Typography variant="h5">¿Otra cosa?</Typography>
-          <Typography variant="h6" noWrap component="div">
-            <br />
-            <p>Otras cosas</p>
-            <p>a</p>a<p>a</p>
-            <p>a</p>
-            <p>a</p>
-          </Typography>
-        </Paper>
-      </Grid>
-    </Slide>
-    <Slide in={true} timeout={500} direction="up">
-      <Grid
-        item
-        xs={6}
-        sx={{ p: 2.5, pl: 5, display: 'flex', alignItems: 'center' }}
-      >
-        <Paper sx={{ width: '100%', height: '100%', p: 2 }}>
-          <Typography variant="h5" id="g1">G1</Typography>
-          <Typography variant="h6" noWrap component="div">
-            <br />
-            <p>Otras cosas</p>
-            <p>a</p>a<p>a</p>
-            <p>a</p>
-            <p>a</p>
-          </Typography>
-        </Paper>
-      </Grid>
-    </Slide>
-    <Slide in={true} timeout={500} direction="right">
-      <Grid
-        item
-        xs={6}
-        sx={{ p: 2.5, pr: 5, display: 'flex', alignItems: 'center' }}
-      >
-        <Paper sx={{ width: '100%', height: '100%', p: 2 }}>
-          <Typography variant="h5">¿Otra cosa?</Typography>
-          <Typography variant="h6" noWrap component="div">
-            <br />
-            <p>Otras cosas</p>
-            <p>a</p>a<p>a</p>
-            <p>a</p>
-            <p>a</p>
-          </Typography>
-        </Paper>
-      </Grid>
-    </Slide>
-    <Slide in={true} timeout={500} direction="up">
-      <Grid
-        item
-        xs={6}
-        sx={{ p: 2.5, pl: 5, display: 'flex', alignItems: 'center' }}
-      >
-        <Paper sx={{ width: '100%', height: '100%', p: 2 }}>
-          <Typography variant="h5">¿Otra cosa?</Typography>
-          <Typography variant="h6" noWrap component="div">
-            <br />
-            <p>Otras cosas</p>
-            <p>a</p>a<p>a</p>
-            <p>a</p>
-            <p>a</p>
-          </Typography>
-        </Paper>
-      </Grid>
-    </Slide>
-    <Slide in={true} timeout={500} direction="right">
-      <Grid
-        item
-        xs={6}
-        sx={{ p: 2.5, pr: 5, display: 'flex', alignItems: 'center' }}
-      >
-        <Paper sx={{ width: '100%', height: '100%', p: 2 }}>
-          <Typography variant="h5">¿Otra cosa?</Typography>
-          <Typography variant="h6" noWrap component="div">
-            <br />
-            <p>Otras cosas</p>
-            <p>a</p>a<p>a</p>
-            <p>a</p>
-            <p>a</p>
-          </Typography>
-        </Paper>
-      </Grid>
-    </Slide>
-  </Grid>
-)
+    <Grid container>
+      <Slide in={true} direction="up">
+        <Grid
+          item
+          xs={4}
+          sx={{
+            p: 5,
+            pr: 2.5,
+            pb: 2.5,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <img src={Logo} style={{ width: '100%' }} alt="Logo Gaia" />
+        </Grid>
+      </Slide>
+      <Slide in={true} timeout={500} direction="left">
+        <Grid
+          item
+          xs={8}
+          sx={{ p: 5, pl: 2.5, pb: 2.5, display: 'flex', alignItems: 'center' }}
+        >
+          <Paper sx={{ width: '100%', height: '100%', p: 2 }}>
+            <Typography variant="h5">¿Que es Gaia?</Typography>
+            <Typography variant="h6" noWrap component="div">
+              <br />
+              <p>
+                Gaia es un sistema de Gestión Administrativa e Innovación
+                Académica, con una amplia gama de funcionalidades y un amplio
+                abanico de posibilidades de personalización. En GAIA buscamos
+                brindarte la mejor de las experiencias de usuario.
+              </p>
+              <p>
+                *G*estionar los distintos espacios institucionales, gracias a
+                nuestro sistema de modulos que te permitirá compartimentar la
+                información de una manera clara e intuitiva.
+              </p>
+              <p>
+                *A*dministrativamente notarás mejoras instantaneas,
+                permitiendote ahorrar tiempo, obtener claridad en el flujo de
+                información, lograr una optimización de los procesos, lo que se
+                traducirá de manera casi inmediata en una mayor rentabilidad
+                para tu negocio.
+              </p>
+              <p>
+                *I*nnovar es el único camino para el éxito, por eso contarás con
+                un equipo totalmente capacitado y comprometido con tu
+                crecimiento como comunidad educativa. Pondremos a tu disposición
+                no tan solo nuestros conocimientos, sino también nuestro carisma
+                y pasión por la excelencia.
+              </p>
+              <p>
+                *A*cademicamente brindarás una mejor experiencia a los miembros
+                de tu comunidad educativa, como así también la sensanción de
+                respaldo, progreso, compromiso, transparencia y dinamismo.
+              </p>
+            </Typography>
+          </Paper>
+        </Grid>
+      </Slide>
+      <Slide in={true} timeout={500} direction="up">
+        <Grid
+          item
+          xs={7}
+          sx={{ p: 2.5, pl: 5, display: 'flex', alignItems: 'center' }}
+        >
+          <Paper sx={{ width: '100%', height: '100%', p: 2 }}>
+            <Typography variant="h5">¿Otra cosa?</Typography>
+            <Typography variant="h6" noWrap component="div">
+              <br />
+              <p>Otras cosas</p>
+              <p>a</p>a<p>a</p>
+              <p>a</p>
+              <p>a</p>
+            </Typography>
+          </Paper>
+        </Grid>
+      </Slide>
+      <Slide in={true} timeout={500} direction="right">
+        <Grid
+          item
+          xs={5}
+          sx={{ p: 2.5, pr: 5, display: 'flex', alignItems: 'center' }}
+        >
+          <Paper sx={{ width: '100%', height: '100%', p: 2 }}>
+            <Typography variant="h5">¿Otra cosa?</Typography>
+            <Typography variant="h6" noWrap component="div">
+              <br />
+              <p>Otras cosas</p>
+              <p>a</p>a<p>a</p>
+              <p>a</p>
+              <p>a</p>
+            </Typography>
+          </Paper>
+        </Grid>
+      </Slide>
+      <Slide in={true} timeout={500} direction="up">
+        <Grid
+          item
+          xs={6}
+          sx={{ p: 2.5, pl: 5, display: 'flex', alignItems: 'center' }}
+        >
+          <Paper sx={{ width: '100%', height: '100%', p: 2 }}>
+            <Typography variant="h5" id="g1">
+              G1
+            </Typography>
+            <Typography variant="h6" noWrap component="div">
+              <br />
+              <p>Otras cosas</p>
+              <p>a</p>a<p>a</p>
+              <p>a</p>
+              <p>a</p>
+            </Typography>
+          </Paper>
+        </Grid>
+      </Slide>
+      <Slide in={true} timeout={500} direction="right">
+        <Grid
+          item
+          xs={6}
+          sx={{ p: 2.5, pr: 5, display: 'flex', alignItems: 'center' }}
+        >
+          <Paper sx={{ width: '100%', height: '100%', p: 2 }}>
+            <Typography variant="h5">¿Otra cosa?</Typography>
+            <Typography variant="h6" noWrap component="div">
+              <br />
+              <p>Otras cosas</p>
+              <p>a</p>a<p>a</p>
+              <p>a</p>
+              <p>a</p>
+            </Typography>
+          </Paper>
+        </Grid>
+      </Slide>
+      <Slide in={true} timeout={500} direction="up">
+        <Grid
+          item
+          xs={6}
+          sx={{ p: 2.5, pl: 5, display: 'flex', alignItems: 'center' }}
+        >
+          <Paper sx={{ width: '100%', height: '100%', p: 2 }}>
+            <Typography variant="h5">¿Otra cosa?</Typography>
+            <Typography variant="h6" noWrap component="div">
+              <br />
+              <p>Otras cosas</p>
+              <p>a</p>a<p>a</p>
+              <p>a</p>
+              <p>a</p>
+            </Typography>
+          </Paper>
+        </Grid>
+      </Slide>
+      <Slide in={true} timeout={500} direction="right">
+        <Grid
+          item
+          xs={6}
+          sx={{ p: 2.5, pr: 5, display: 'flex', alignItems: 'center' }}
+        >
+          <Paper sx={{ width: '100%', height: '100%', p: 2 }}>
+            <Typography variant="h5">¿Otra cosa?</Typography>
+            <Typography variant="h6" noWrap component="div">
+              <br />
+              <p>Otras cosas</p>
+              <p>a</p>a<p>a</p>
+              <p>a</p>
+              <p>a</p>
+            </Typography>
+          </Paper>
+        </Grid>
+      </Slide>
+    </Grid>
+  )
 }
--- src/components/LandingPage/Who.jsx
+++ src/components/LandingPage/Who.jsx
@@ -1,15 +1,84 @@
 import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
-import Grow from '@mui/material/Grow';
+import Grow from '@mui/material/Grow'
 
-export default function Who(){
+export default function Who() {
   return (
-  <Box sx={{ p: 5, height: 'calc(100vh - 64px)' }}>
-    <Grow in={true} timeout={500}>
-    <Paper elevation={24} sx={{ width: '100%', height: '100%', p: 2 }}>
-      Somos...
-    </Paper>
-    </Grow>
-  </Box>
-)
+    <Box sx={{ p: 5, height: 'calc(100vh - 64px)' }}>
+      <Grow in={true} timeout={500}>
+        <Paper elevation={24} sx={{ width: '100%', height: '100%', p: 2 }}>
+          <p>
+            GAIA es el resultado de un proyecto grupal de estudiantes de Henry
+            (link a henry), que combinaron por un lado los conocimientos
+            adquiridos durante el bootcamp, y por otro la habilidad de adquirir
+            nuevos conocmientos a un ritmo mucho mayor de lo normal. Luego de
+            muchas semanas repletas de trabajo, risas, estrechar vinculos,
+            re-descubrir nuestras habilidades y crecimiento académico y
+            profesional, nació GAIA. Este es el humilde equipo que trabajó con
+            mucha pasión:
+          </p>
+          <p>Edwin Arias</p>
+          <a href="https://github.com/ferwinred">GitHub Profile</a>
+          <a href="https://www.linkedin.com/in/ferwinarias/">
+            LinkedIn Profile
+          </a>
+          Medellín, Antioquia, Colombia
+          <br></br>
+          <p>Valentin Campos Nualart</p>
+          <a href="https://github.com/ValentinCN">GitHub Profile</a>
+          <a href="https://www.linkedin.com/in/valentin-campos-nualart-2b6362206/">
+            LinkedIn Profile
+          </a>
+          Partido de Quilmes, Provincia de Buenos Aires, Argentina
+          <br></br>
+          <p>Juan Daniele</p>
+          <a href="https://github.com/Juandaniele">GitHub Profile</a>
+          <a href="https://www.linkedin.com/in/juan-cruz-daniele/">
+            LinkedIn Profile
+          </a>
+          Cordoba, Argentina
+          <br></br>
+          <p>Luan Klett</p>
+          <a href="https://github.com/LuanKlett">GitHub Profile</a>
+          <a href="https://www.linkedin.com/in/luan-klett/">LinkedIn Profile</a>
+          Rosario, Santa Fe, Argentina
+          <br></br>
+          <p>Nicolás Liasso</p>
+          <a href="https://github.com/NLiasso">GitHub Profile</a>
+          <a href="https://www.linkedin.com/in/nicolas-liasso/">
+            LinkedIn Profile
+          </a>
+          Santiago del Estero, Argentina
+          <br></br>
+          <p>Wilmer Ortiz Castillo</p>
+          <a href="https://github.com/wilmerortiz">GitHub Profile</a>
+          <a href="https://www.linkedin.com/in/wilmer-ortiz-castillo/">
+            LinkedIn Profile
+          </a>
+          Morales, San Martín, Perú
+          <br></br>
+          <p>Jose Sanchez</p>
+          <a href="https://github.com/JRS-Developer">GitHub Profile</a>
+          <a href="https://www.linkedin.com/in/jose-s-developer/">
+            LinkedIn Profile
+          </a>
+          Falcón, Venezuela
+          <br></br>
+          <p>Leandro Villafuerte</p>
+          <a href="https://github.com/LeandroVillafuerte">GitHub Profile</a>
+          <a href="https://www.linkedin.com/in/leandrovillafuerte/ ">
+            LinkedIn Profile
+          </a>
+          San Miguel de Tucumán, Tucumán, Argentina
+          <br></br>
+          <p>Ignacio Amatt (HM)</p>
+          <a href="https://github.com/nachovip">GitHub Profile</a>
+          <a href="https://www.linkedin.com/in/ignacio-amatt/">
+            LinkedIn Profile
+          </a>
+          Buenos Aires, Argentina
+        </Paper>
+      </Grow>
+    </Box>
+  )
 }
